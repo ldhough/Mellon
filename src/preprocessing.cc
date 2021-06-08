6f3bd2a49acd85e1dc4eed49e3780bd166807480
ldhough <ldhough2000@gmail.com>
2021-06-07 22:04:19 -0400

    saving work for minor code addition to parallelize preprocessing step

--- src/preprocessing.cc
+++ src/preprocessing.cc
@@ -5,6 +5,7 @@
 #include <utility>
 #include <iostream>
 #include <thread>
+#include <cmath>
 
 using namespace std;
 
@@ -36,7 +37,21 @@ void Password_Preprocessor::sort_by_lev_dist() {
 
 vector<pair<string, int>> Password_Preprocessor::process() {
 
+    /*
+     * We want to compute the minimum levenshtein distance from one password to another in the set for each password.  
+     * Because the distance is symmetric each password i only needs to be compared to passwords i+1 -> n.  To parallelize
+     * this well, we want to know the total number of distances to calculate.  The number of distances computed can be
+     * expressed as ~~ sum of i from i=0 to n-1 ~~ where n is the size of the list.  One password is compared to no
+     * passwords, one is compared to one passwords ... one is compared to n-1 passwords. Closed form is (1/2)(n-1)n
+     */ 
+
+    int n = this->list_size;
+    // long is needed to hold number of computations needed for large password lists which exceed INT_MAX
+    long num_distances = ceil(0.5*(n-1)*n);
+
     int num_cores = thread::hardware_concurrency();
+
+    long distances_per_core = num_distances / num_cores;
 
     this->calculate_min_lev(0);
     return this->password_distances;
